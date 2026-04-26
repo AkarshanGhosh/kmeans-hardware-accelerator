9745b833c18d9d16034074667ef5c087b4d8a216
AkarshanGhosh <ronighosh494@gmail.com>
2026-04-26 12:54:38 +0530

    verilog hardware code
    distance_unit.v

---
+++ verilog/kmeans_accelerator/kmeans_accelerator.srcs/sources_1/new/distance_unit.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/26/2026 12:52:58 PM
+// Design Name: 
+// Module Name: distance_unit
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module distance_unit(
+
+    );
+endmodule
